1c2e300ea010355a6eeacc15c94d256cb60b1f02
Guilherme Fernades <guilhermegfg2011@hotmail.com>
2025-11-03 21:52:09 -0300

    desafio Crie Seu Monstro favorito

---
+++ 002-crieSeuMonstroFavorito/002.py
@@ -0,0 +1,7 @@
+print("Vamos criar o seu Monstro Favorito...\n")
+nome = input("Digite o nome do seu monstro favorito: ")
+tipo = input("digite o tipo (ex: dragão, goblin...): ")
+força = int(input("Digite um nível de força de 1 a 100: "))
+habilidade = input("Digite qual habilidade especial ele possui: ")
+
+print (f"O monstro {nome} é um {tipo} de nível {força} e possui a habilidade especial: {habilidade}")
